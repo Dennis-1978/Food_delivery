75ec58fd1b23494eac09cc7e4110be7468d140a6
Denis <d.khokhloff2011@yandex.ru>
2021-07-01 16:58:06 +0300

    upgrading a request to the server using fetch

--- js/script.js
+++ js/script.js
@@ -253,22 +253,56 @@ window.addEventListener("DOMContentLoaded", () => {
             `;
             form.insertAdjacentElement("afterend", statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open("POST", "server.php");
-            // request.setRequestHeader("Content-type", "multipart/form-data");
 
+            // // Запрос с FormData
+            // const formData = new FormData(form);
+
+            // fetch("server.php", {
+            //     method: "POST",
+            //     // headers: {
+            //     //     "Content-type": "application/json"
+            //     // },
+            //     body: formData
+            // })
+            // .then(data => data.text())
+            // .then(data => {
+            //     console.log(data);
+            //     showThanksModal(message.success);
+            //     statusMessage.remove();
+            // })
+            // .catch(() => {
+            //     showThanksModal(message.failure);
+            // })
+            // .finally(() => {
+            //     form.reset();
+            // });
+
+            // Запрос с JSON
             const formData = new FormData(form);
-            request.send(formData);
 
-            request.addEventListener("load", () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            const object = {};
+                formData.forEach(function(value, key) {
+                    object[key] = value;
+                });
+
+            fetch("server.php", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
         });
     }
@@ -299,5 +333,4 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }, 4000);
     }
-
 });
